2017d5330ded447817e0a3b0f3269fc25fa6a2d1
Daniel Costa <danieldaleva@gmail.com>
2020-08-03 18:51:24 -0300

    remove sucrase added ts-node-dev

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -9,7 +9,7 @@ class UserController {
     return res.json(users)
   }
 
-  public async store(req: Request, res: Response): Promise<Response> {
+  public async store (req: Request, res: Response): Promise<Response> {
     const user = await User.create(req.body)
 
     return res.json(user)
